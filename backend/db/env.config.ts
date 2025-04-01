d40bc582f19601ace84d21d44ac06401400f4d25
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-04-01 13:01:55 +0100

    add tsnode and gitignore

--- backend/db/env.config.ts
+++ backend/db/env.config.ts
@@ -8,7 +8,7 @@ import 'dotenv/config'
 export const config = {
   development: {
     username: process.env.MYSQL_USER_DEV || 'root',
-    password: process.env.MYSQL_PASSWORD_DEV || 'root',
+    password: process.env.MYSQL_PASSWORD_DEV || '1234',
     database: process.env.MYSQL_DATABASE_DEV || 'test',
     host: process.env.DB_HOST || 'localhost',
   }
