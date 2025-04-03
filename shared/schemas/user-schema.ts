c5e3a8716e4e713bb0bebf9d8ad1ad8b462061b1
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-04-03 11:54:31 +0200

    User adapted to schema in progress

---
+++ shared/schemas/user-schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  id: z.number().optional(),
+  name: z.string().min(1, { message: "Name is required" }),
+  lastname: z.string().min(1, { message: "Lastname is required" }),
+  password: z.string().min(1, { message: "Password is required" }),
+  email: z.string().min(1, { message: "Email is required" }),
+});
