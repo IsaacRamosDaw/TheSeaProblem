b0e22b83b43ca30cf3b4c44807309cc5c4f5b277
Echedey Henri <echedeyhh1989@gmail.com>
2025-04-07 20:01:48 +0200

    add pollution enum to chart-colors

--- frontend/src/constants/chart-colors.ts
+++ frontend/src/constants/chart-colors.ts
@@ -1,4 +1,9 @@
-export const COLORS: Record<string, { bg: string; border: string }> = {
+import { PollutionEnum } from "@/shared/schemas/report-schema";
+import { z } from "zod";
+
+type PollutionType = z.infer<typeof PollutionEnum>;
+
+export const COLORS: Record<PollutionType, { bg: string; border: string }> = {
   Chemical: { bg: "rgba(75,192,192,0.4)", border: "rgba(75,192,192,1)" },
   Plastic: { bg: "rgba(255,99,132,0.4)", border: "rgba(255,99,132,1)" },
   "Oil Spill": { bg: "rgba(255,206,86,0.4)", border: "rgba(255,206,86,1)" },
