7e4eaf25fd4773b5dafa1b2c5bfed41cfcf4a33c
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-04-08 14:53:21 +0200

    users reports in progress

--- frontend/src/components/landing/usersReportsContainer/UserReportsContainer.tsx
+++ frontend/src/components/landing/usersReportsContainer/UserReportsContainer.tsx
@@ -4,7 +4,7 @@ import s from './userReportsContainer.module.scss';
 export function UserReportsContainer(){
   return (
     <section className={s.userReportsContainer}>
-      <UserReport/>
+      <UserReport id={1} location="Las palmas" img="LogoSimple.svg" alt="alt-image" date="10-12-12" Pollution="Plastic" shortDescription="short Description"/>
     </section>
   )
 }
