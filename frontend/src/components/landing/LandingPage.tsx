478d615f9e4c4dc01e7c697342a1e9e98ca007f8
Isaac <164216296+IsaacRamosDaw@users.noreply.github.com>
2025-04-08 12:41:52 +0100

    68 related topics (#69)
    * footer scss minor fix done
    
    * Topics and topics container created
    
    * Topics container added to landing page
    
    * topics added and first tyles done
    
    * topics added and first tyles done v2

--- frontend/src/components/landing/LandingPage.tsx
+++ frontend/src/components/landing/LandingPage.tsx
@@ -5,16 +5,18 @@ import { Footer } from "../general/footer/Footer";
 import { StaticReports } from "./staticReports/StaticReports";
 import EmissionsChart from "../emissionsChart/EmissionsChart";
 import styles from "./landingPage.module.scss";
+import { RelatedTopics } from "./relatedTopics/RelatedTopics";
 
 export function LandingPage() {
   return (
     <div className={styles.landingPage}>
       <Header />
       <StaticReports/>
-      <Footer />
       {/* I put it here to be able to see that the graph was displayed correctly. */}
       <h1>Bienvenido al Dashboard de The Sea Problem</h1>
+      <RelatedTopics/>
       <EmissionsChart />
+      <Footer />
     </div>
   );
 }
