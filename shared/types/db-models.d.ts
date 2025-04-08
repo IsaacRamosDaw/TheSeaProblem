83c21bc6305f64a6112475fdd77b55cb70eb752c
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-04-08 10:59:10 +0100

    fix typo in name

--- shared/types/db-models.d.ts
+++ shared/types/db-models.d.ts
@@ -3,11 +3,11 @@ import { PollutionEnum, ReportSchema } from "../schemas/report-schema";
 import { UsersSchema } from "../schemas/user-schema";
 import { CompanySchema } from "../schemas/company-schema";
 import { EmissionsSchema } from "../schemas/emission-schema";
-import { StatictReporSchema } from "../schemas/StaticReport-Schema";
+import { StaticReportSchema } from "../schemas/StaticReport-Schema";
 
 export type User = z.infer<typeof UsersSchema>;
 export type Report = z.infer<typeof ReportSchema>;
 export type PollutionType = z.infer<typeof PollutionEnum>;
 export type Company = z.infer<typeof CompanySchema>;
 export type Emission = z.infer<typeof EmissionsSchema>;
-export type StaticReport = z.infer<typeof StatictReportSchema>;
+export type StaticReport = z.infer<typeof StaticReportSchema>;
