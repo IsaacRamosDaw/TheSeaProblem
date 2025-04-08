5ce4a752f391435eb2fa217616462c59d61f9fd2
Isaac <164216296+IsaacRamosDaw@users.noreply.github.com>
2025-04-08 10:32:09 +0100

    50 our static reports (#61)
    * Header started
    
    * Header in progress
    
    * Footer in progress
    
    * staticReports in progress
    
    * staticReports in almost done
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Luke Hening <152434106+utlandingur@users.noreply.github.com>

---
+++ frontend/src/components/general/footer/Footer.tsx
@@ -0,0 +1,30 @@
+import "./footer.scss"
+import { FaYoutube } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { RiInstagramFill } from "react-icons/ri";
+import { AiFillTikTok } from "react-icons/ai";
+
+export function Footer() {
+  return(
+    <footer>
+        <div id="social-media">
+        <a href="">
+          <FaYoutube className="icon" />
+          <span>text</span>
+        </a>
+        <a href="">
+          <BsTwitterX className="icon" />
+          <span>text</span>
+        </a>
+        <a href="">
+          <RiInstagramFill className="icon" />
+          <span>text</span>
+        </a>
+        <a href="">
+          <AiFillTikTok className="icon" />
+          <span>text</span>
+        </a>
+      </div>
+    </footer>
+  )
+}
--- frontend/src/components/landing/LandingPage.tsx
+++ frontend/src/components/landing/LandingPage.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import type { Report } from "@/shared/types/db-models";
+import { Header } from "../general/header/Header";
+import { Footer } from "../general/footer/Footer";
+import { StaticReports } from "./staticReports/StaticReports";
 import EmissionsChart from "../emissionsChart/EmissionsChart";
 import styles from "./landingPage.module.scss";
-//! Ten al lado el services/ourReports.services.js
+
 export function LandingPage() {
   const [ourReports, setOurReports] = useState<Report[]>([]);
   const [text, setText] = useState<string>();
+
   // useEffect(() => {
   //   (async function fetchData() {
   //     const data = await getOurReports();
@@ -15,6 +19,9 @@ export function LandingPage() {
 
   return (
     <div className={styles.landingPage}>
+      <Header />
+      <StaticReports/>
+      <Footer />
       {/* I put it here to be able to see that the graph was displayed correctly. */}
       <h1>Bienvenido al Dashboard de The Sea Problem</h1>
       <EmissionsChart />
