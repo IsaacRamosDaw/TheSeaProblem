f37764f63c15dac656f04b578cf1dbe8e98ed138
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-04-08 11:03:54 +0100

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <Router>
       <Routes>
-      {/* Cree un componente que ya tiene las llamadas de servicios  */}
-      {/* Ve a components/landing/LandingPage.jsx  */}
-        <Route path="*" element={<LandingPage />}/>
-        <Route path="/" element={<LandingPage />}/>
+        {/* Cree un componente que ya tiene las llamadas de servicios  */}
+        {/* Ve a components/landing/LandingPage.jsx  */}
+        {/* <Route path="*" element={<LandingPage />}/> */}
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/landing/LandingPage.tsx
+++ frontend/src/components/landing/LandingPage.tsx
@@ -6,6 +6,8 @@ import { StaticReports } from "./staticReports/StaticReports";
 import EmissionsChart from "../emissionsChart/EmissionsChart";
 import styles from "./landingPage.module.scss";
 
+import { login, logout } from "../../services/auth";
+//! Ten al lado el services/ourReports.services.js
 export function LandingPage() {
   const [ourReports, setOurReports] = useState<Report[]>([]);
   const [text, setText] = useState<string>();
@@ -20,11 +22,13 @@ export function LandingPage() {
   return (
     <div className={styles.landingPage}>
       <Header />
-      <StaticReports/>
+      <StaticReports />
       <Footer />
       {/* I put it here to be able to see that the graph was displayed correctly. */}
       <h1>Bienvenido al Dashboard de The Sea Problem</h1>
       <EmissionsChart />
+      <button onClick={login}>login</button>
+      <button onClick={logout}>logout</button>
     </div>
   );
 }
