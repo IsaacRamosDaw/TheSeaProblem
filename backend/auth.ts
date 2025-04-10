17b1e214dd1076911e003cd75df80afa5d25c094
Echedey Henri <echedeyhh1989@gmail.com>
2025-04-10 09:18:34 +0200

    use auth

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,9 +1,11 @@
 // Authorization middleware. When used, the Access Token must
 import { auth } from "express-oauth2-jwt-bearer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // exist and be verified against the Auth0 JSON Web Key Set.
 export const checkJwt = auth({
-  audience: process.env.AUDIENCE || "http://localhost:8080",
-  issuerBaseURL:
-    process.env.ISSUER_BASE_URL || "https://dev-gqjdn37m0smuisy7.us.auth0.com",
+  audience: process.env.VITE_AUDIENCE,
+  issuerBaseURL: `https://${process.env.VITE_DOMAIN}`,
 });
