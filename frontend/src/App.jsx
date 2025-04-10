33471692465ffc57f16bd122eebc3f654c1c8cac
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-04-10 12:09:35 +0200

    fixed to new auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "./components/landing/LandingPage";
-import { AuthProvider } from "./context/AuthContext";
 import { CompanyList, AddCompany, CompanyDetail } from "./components/companies";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 function App() {
   return (
-    <Auth0Provider
-      domain={import.meta.env.VITE_DOMAIN}
-      clientId={import.meta.env.VITE_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: import.meta.env.VITE_LOGIN_REDIRECT_URI,
-        audience: import.meta.env.VITE_AUDIENCE
-      }}
-    >
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />}/>
@@ -24,7 +14,6 @@ function App() {
           <Route path="*" element={<LandingPage />}/>
         </Routes>
       </Router>
-    </Auth0Provider>
   );
 }
 
