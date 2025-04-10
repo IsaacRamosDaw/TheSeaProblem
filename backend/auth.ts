6b666388e6463d713113d8fa4bc3fac6a1c76022
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2025-04-10 13:43:59 +0100

    fix dev

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,11 +1,8 @@
 // Authorization middleware. When used, the Access Token must
 import { auth } from "express-oauth2-jwt-bearer";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // exist and be verified against the Auth0 JSON Web Key Set.
 export const checkJwt = auth({
-  audience: process.env.VITE_AUDIENCE,
-  issuerBaseURL: `https://${process.env.VITE_DOMAIN}`,
+  audience: process.env.AUDIENCE,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
 });
